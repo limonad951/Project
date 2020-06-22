d7a5782e1dc4f64f66b2952ca498f09350b426db
Tolik <limonad9802070@gmail.com>
2020-06-22 14:28:04 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+    "use strict";
+
+    // const isChecked = true,
+    //       isClose = false;
+
+    // console.log(isChecked || !isClose);
+
+    // console.log('arr' + " - object");
+    // console.log(4 + "5");
+
+    // let incr = 10,       
+    //     decr = 10;
+
+    // incr++;  //or ++incr;
+    // decr--;  //or --decr;
+
+    // console.log(incr);
+    // console.log(decr);
+
+    const category = 'toys';
+    console.log(`https://someurl.com/${category}/5`);
+
+    // const user = "Ivan";
+
+    // alert(`Hello, ${user}`);
+
+    // const answers = [];
+
+    // answers[0] = prompt('What is your name?', "");
+    // answers[1] = prompt('How old are you?', "");
+
+    // console.log(typeof(answers));
+
+    // const answer = +prompt("Ты тут?", "");
+    // console.log(answer + 5);
+
+    // let number = 4.6;
+
+    // console.log(4/0);
+    // console.log('string' * 9);
+
+    // const persone = 'Alex';
+
+    // const bool = true; // false
+
+    // // console.log(something); // null
+
+    // let und;
+    // console.log(und); // undefined
+
+    // const obj = {
+    //     name: "John",
+    //     age: 25,
+    //     isMarried: false
+    // };      
+
+    // console.log(obj.name); // or console.log(obj["name"]);
+
+    // let arr = ['123.png', '222.jpg',6 ,'apple.png', {}, []];
+    // console.log(arr[1]);
+
+    
+    // let number = 5;
+    // const leftBorderWidth = 1;
+
+    // number = 10;
+    // console.log(number);
+
+    // const obj = {
+    //     a: 50
+    // };
+    
+    // obj.a = 10;
+
+    // console.log(obj);
+
+    // var name = 'Pupok'; // старое объявление - не использовать! Используй let or const!
+
+
+    // {
+    //     let result = 50;
+    //     console.log(result);
+    // }
